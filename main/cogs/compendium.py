b65e4e536e01ca2d299c41e8f3e280a7ae3f1c5f
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-07-19 18:00:24 -0700

    Added compendium.py

---
+++ main/cogs/compendium.py
@@ -0,0 +1,57 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                         Imports
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from __future__ import annotations
+
+# Standard library imports
+import logging
+import re
+import string
+
+from typing import TYPE_CHECKING, Optional
+from unicodedata import category
+
+# Third party imports
+import discord  # noqa
+from async_lru import alru_cache
+from discord import app_commands
+from discord.ext import commands
+
+# Local application imports
+if TYPE_CHECKING:
+    from main.Zen import Zen
+    from main.cogs.utils.context import Context
+
+
+log = logging.getLogger(__name__)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                         Compendium
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Compendium(commands.Cog):
+    def __init__(self, bot: Zen) -> None:
+        self.bot: Zen = bot
+
+    @property
+    def display_emoji(self) -> discord.PartialEmoji:
+        return discord.PartialEmoji(name='\N{VIDEO GAME}')
+
+    # ====================================================
+    # Commands
+    @app_commands.command(name='background')
+    @app_commands.describe(query='Background')
+    async def background(
+        self,
+        interaction: discord.Interaction,
+        query: string
+    ):
+        """ Looks up a background """
+        await interaction.response.defer()
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                         Setup
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+async def setup(bot: Zen):
+    await bot.add_cog(Compendium(bot))
