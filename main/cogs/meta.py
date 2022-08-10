2cc6f024a405dec40316a1db1a0a709c38f16d20
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-08-10 00:16:44 -0700

    Ready for release

--- main/cogs/meta.py
+++ main/cogs/meta.py
@@ -63,7 +63,8 @@ class Meta(commands.Cog):
     async def info(self, interaction: discord.Interaction) -> None:
         """ Helpful Information """
         e = discord.Embed(title='Help', color=discord.Colour.random())
-        e.description = 'For detailed information about the bot please view the github page linked below.'
+        e.description = 'For detailed information about the bot please view the github page linked below.\n\n'
+        e.description += 'Use the invite command to invite the bot to your server.'
         e.timestamp = discord.utils.utcnow()
         e.add_field(
             name='Discord Server',
@@ -81,7 +82,30 @@ class Meta(commands.Cog):
             inline=False
         )
 
-        interaction.response.send_message(embed=e)
+        await interaction.response.send_message(embed=e)
+
+    @app_commands.command()
+    async def invite(self, interaction: discord.Interaction) -> None:
+        """ Joins a server. """
+        perms = discord.Permissions.none()
+        perms.read_messages = True
+        perms.change_nickname = True
+        perms.send_messages = True
+        perms.send_messages_in_threads = True
+        perms.create_public_threads = True
+        perms.create_private_threads = True
+        perms.manage_messages = True
+        perms.manage_threads = True
+        perms.embed_links = True
+        perms.attach_files = True
+        perms.read_message_history = True
+        perms.use_external_emojis = True
+        perms.use_external_stickers = True
+        perms.use_application_commands = True
+
+        await interaction.response.send_message(
+            f'<{discord.utils.oauth_url(self.bot.client_id, permissions=perms)}>'
+        )
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
