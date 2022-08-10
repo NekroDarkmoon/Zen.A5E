6e61f82802db6da9bdf43a0a8d01c9b0297d1992
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-08-09 23:04:42 -0700

    Removed redundant code and added a helper

--- main/cogs/meta.py
+++ main/cogs/meta.py
@@ -41,278 +41,11 @@ log = logging.getLogger('__name__')
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                  Group Help Page Source
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-class GroupHelpPageSource(menus.ListPageSource):
-    def __init__(
-        self,
-        group: Union[commands.Group,
-                     commands.Cog],
-        commands: list[commands.Command],
-        *,
-        prefix: str
-    ) -> None:
-        super().__init__(entries=commands, per_page=6)
-        self.group: Union[commands.Group, commands.Cog] = group
-        self.prefix: str = prefix
-        self.title: str = f'{self.group.qualified_name} Commands'
-        self.description: str = self.group.description
-
-    async def format_page(self, menu: ZenPages, commands: list[commands.Command]):
-        embed = discord.Embed(
-            title=self.title, description=self.description, colour=discord.Colour(0xA8B9CD))
-
-        for command in commands:
-            signature = f'{command.qualified_name} {command.signature}'
-            embed.add_field(
-                name=signature, value=command.short_doc or 'No help given...', inline=False)
-
-        maximum = self.get_max_pages()
-        if maximum > 1:
-            embed.set_author(
-                name=f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} commands)')
-
-        embed.set_footer(
-            text=f'Use "{self.prefix}help command" for more info on a command.')
-        return embed
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                     Help Select Menu
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-class HelpSelectMenu(discord.ui.Select['HelpMenu']):
-    def __init__(self, commands: dict[commands.Cog, list[commands.Command]], bot: Zen):
-        super().__init__(
-            placeholder='Select a category...',
-            min_values=1,
-            max_values=1,
-            row=0,
-        )
-        self.commands: dict[commands.Cog, list[commands.Command]] = commands
-        self.bot: Zen = bot
-        self.__fill_options()
-
-    def __fill_options(self) -> None:
-        self.add_option(
-            label='Index',
-            emoji='\N{WAVING HAND SIGN}',
-            value='__index',
-            description='The help page showing how to use the bot.',
-        )
-        for cog, commands in self.commands.items():
-            if not commands:
-                continue
-            description = cog.description.split('\n', 1)[0] or None
-            emoji = getattr(cog, 'display_emoji', None)
-            self.add_option(label=cog.qualified_name, value=cog.qualified_name,
-                            description=description, emoji=emoji)
-
-    async def callback(self, interaction: discord.Interaction):
-        assert self.view is not None
-        value = self.values[0]
-        if value == '__index':
-            await self.view.rebind(FrontPageSource(), interaction)
-        else:
-            cog = self.bot.get_cog(value)
-            if cog is None:
-                await interaction.response.send_message('Somehow this category does not exist?', ephemeral=True)
-                return
-
-            commands = self.commands[cog]
-            if not commands:
-                await interaction.response.send_message('This category has no commands for you', ephemeral=True)
-                return
-
-            source = GroupHelpPageSource(
-                cog, commands, prefix=self.view.ctx.clean_prefix)
-            await self.view.rebind(source, interaction)
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                       Front Page Source
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-class FrontPageSource(menus.PageSource):
-    def is_paginating(self) -> bool:
-        # This forces the buttons to appear even in the front page
-        return True
-
-    def get_max_pages(self) -> Optional[int]:
-        # There's only one actual page in the front page
-        # However we need at least 2 to show all the buttons
-        return 2
-
-    async def get_page(self, page_number: int) -> Any:
-        # The front page is a dummy
-        self.index = page_number
-        return self
-
-    def format_page(self, menu: HelpMenu, page: Any):
-        embed = discord.Embed(
-            title='Bot Help', colour=discord.Colour(0xA8B9CD))
-        embed.description = inspect.cleandoc(
-            f"""
-            Hello! Welcome to the help page.
-            Use "{menu.ctx.clean_prefix}help command" for more info on a command.
-            Use "{menu.ctx.clean_prefix}help category" for more info on a category.
-            Use the dropdown menu below to select a category.
-        """
-        )
-
-        if self.index == 0:
-            embed.add_field(
-                name='Who are you?',
-                value=('Someone'),
-                inline=False,
-            )
-        elif self.index == 1:
-            entries = (
-                ('<argument>', 'This means the argument is __**required**__.'),
-                ('[argument]', 'This means the argument is __**optional**__.'),
-                ('[A|B]', 'This means that it can be __**either A or B**__.'),
-                (
-                    '[argument...]',
-                    'This means you can have multiple arguments.\n'
-                    'Now that you know the basics, it should be noted that...\n'
-                    '__**You do not type in the brackets!**__',
-                ),
-            )
-
-            embed.add_field(name='How do I use this bot?',
-                            value='Reading the bot signature is pretty simple.')
-
-            for name, value in entries:
-                embed.add_field(name=name, value=value, inline=False)
-
-        return embed
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                        Help Menu
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-class HelpMenu(ZenPages):
-    def __init__(self, source: menus.PageSource, ctx: Context) -> None:
-        super().__init__(source, ctx=ctx, compact=True)
-
-    def add_categories(self, commands: dict[commands.Cog, list[commands.Command]]) -> None:
-        self.clear_items()
-        self.add_item(HelpSelectMenu(commands, self.ctx.bot))
-        self.fill_items()
-
-    async def rebind(self, source: menus.PageSource, interaction: discord.Interaction) -> None:
-        self.source = source
-        self.current_page = 0
-
-        await self.source._prepare_once()
-        page = await self.source.get_page(0)
-        kwargs = await self._get_kwargs_from_page(page)
-        self._update_labels(0)
-        await interaction.response.edit_message(**kwargs, view=self)
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#                  Paginated Help Command
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-class PaginatedHelpCommand(commands.HelpCommand):
-    context: Context
-
-    def __init__(self):
-        super().__init__(
-            command_attrs={
-                'cooldown': commands.CooldownMapping.from_cooldown(1, 3.0, commands.BucketType.member),
-                'help': 'Shows help about the bot, a command, or a category',
-            }
-        )
-
-    async def on_help_command_error(self, ctx: Context, error: commands.CommandError):
-        if isinstance(error, commands.CommandInvokeError):
-            # Ignore missing permission errors
-            if isinstance(error.original, discord.HTTPException) and error.original.code == 50013:
-                return
-
-            await ctx.send(str(error.original))
-
-    def get_command_signature(self, command: commands.Command) -> str:
-        parent = command.full_parent_name
-        if len(command.aliases) > 0:
-            aliases = '|'.join(command.aliases)
-            fmt = f'[{command.name}|{aliases}]'
-            if parent:
-                fmt = f'{parent} {fmt}'
-            alias = fmt
-        else:
-            alias = command.name if not parent else f'{parent} {command.name}'
-        return f'{alias} {command.signature}'
-
-    async def send_bot_help(self, mapping):
-        bot = self.context.bot
-
-        def key(command) -> str:
-            cog = command.cog
-            return cog.qualified_name if cog else '\U0010ffff'
-
-        entries: list[commands.Command] = await self.filter_commands(bot.commands, sort=True, key=key)
-
-        all_commands: dict[commands.Cog, list[commands.Command]] = {}
-        for name, children in itertools.groupby(entries, key=key):
-            if name == '\U0010ffff':
-                continue
-
-            cog = bot.get_cog(name)
-            assert cog is not None
-            all_commands[cog] = sorted(
-                children, key=lambda c: c.qualified_name)
-
-        menu = HelpMenu(FrontPageSource(), ctx=self.context)
-        menu.add_categories(all_commands)
-        await self.context.release()
-        await menu.start()
-
-    async def send_cog_help(self, cog):
-        entries = await self.filter_commands(cog.get_commands(), sort=True)
-        menu = HelpMenu(GroupHelpPageSource(
-            cog, entries, prefix=self.context.clean_prefix), ctx=self.context)
-        await self.context.release()
-        await menu.start()
-
-    def common_command_formatting(self, embed_like, command):
-        embed_like.title = self.get_command_signature(command)
-        if command.description:
-            embed_like.description = f'{command.description}\n\n{command.help}'
-        else:
-            embed_like.description = command.help or 'No help found...'
-
-    async def send_command_help(self, command):
-        # No pagination necessary for a single command.
-        embed = discord.Embed(colour=discord.Colour(0xA8B9CD))
-        self.common_command_formatting(embed, command)
-        await self.context.send(embed=embed)
-
-    async def send_group_help(self, group):
-        subcommands = group.commands
-        if len(subcommands) == 0:
-            return await self.send_command_help(group)
-
-        entries = await self.filter_commands(subcommands, sort=True)
-        if len(entries) == 0:
-            return await self.send_command_help(group)
-
-        source = GroupHelpPageSource(
-            group, entries, prefix=self.context.clean_prefix)
-        self.common_command_formatting(source, group)
-        menu = HelpMenu(source, ctx=self.context)
-        await self.context.release()
-        await menu.start()
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                        Meta Cog
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class Meta(commands.Cog):
     def __init__(self, bot: Zen) -> None:
         self.bot: Zen = bot
-        self.old_help_command: Optional[commands.HelpCommand] = bot.help_command
-        bot.help_command = PaginatedHelpCommand()
-        bot.help_command.cog = self
 
     def cog_unload(self) -> None:
         self.bot.help_command = self.old_help_command
@@ -324,7 +57,31 @@ class Meta(commands.Cog):
     @app_commands.command()
     async def ping(self, interaction: discord.Interaction) -> None:
         """Ping commands are stupid."""
-        await interaction.response.send_message("Ping commands are stupid.")
+        await interaction.response.send_message("Pong!")
+
+    @app_commands.command()
+    async def info(self, interaction: discord.Interaction) -> None:
+        """ Helpful Information """
+        e = discord.Embed(title='Help', color=discord.Colour.random())
+        e.description = 'For detailed information about the bot please view the github page linked below.'
+        e.timestamp = discord.utils.utcnow()
+        e.add_field(
+            name='Discord Server',
+            value='https://discord.com/invite/r6ufkcpSvU',
+            inline=False
+        )
+        e.add_field(
+            name='Github',
+            value='https://github.com/NekroDarkmoon/Zen.A5E',
+            inline=False
+        )
+        e.add_field(
+            name='Support The Bot',
+            value='[Ko-fi](https://ko-fi.com/nekrodarkmoon)',
+            inline=False
+        )
+
+        interaction.response.send_message(embed=e)
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -339,5 +96,7 @@ class Meta(commands.Cog):
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                         Setup
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 async def setup(bot: Zen) -> None:
     await bot.add_cog(Meta(bot))
